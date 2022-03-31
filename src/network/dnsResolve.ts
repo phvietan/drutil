78dd812970beed2696e5e200de417f5dff724acc
drchain <phvietan@gmail.com>
2022-03-31 20:39:43 +0800

    add jsdoc for dnsResolve

--- src/network/dnsResolve.ts
+++ src/network/dnsResolve.ts
@@ -1,5 +1,12 @@
 import dns from 'dns';
 
+/**
+ * Resolve DNS of hostname to ipv4
+ * In case of error, it returns empty string
+ *
+ * @param {string} hostname - Hostname to be resolved
+ * @return {Promise<string>}
+ */
 export async function dnsResolve(hostname: string): Promise<string> {
   return new Promise<string>((resolve) => {
     dns.lookup(hostname, 4, (err, addr) => {
